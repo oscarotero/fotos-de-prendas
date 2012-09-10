d0d4b35272d2faa7e9892fb41103056473c7491a
Óscar Otero <oom@oscarotero.com>
2012-09-10 15:00:24 +0200

    correxido o texto informativo do formulario de subida

--- apps/Galerias/templates/body-galeria.php
+++ apps/Galerias/templates/body-galeria.php
@@ -1,12 +1,16 @@
+<?php
+$max = intval(ini_get('upload_max_filesize'));
+?>
+
 <section class="galeria">
 	<header>
 		<h1><a href="<?php echo $this->App->url; ?>"><?php echo $this->Controller->titulo; ?></a> / <?php echo $galeria['nome']; ?></h1>
 		<a id="engadir-fotos-boton" class="boton" href="#">Engadir fotos...</a>
 	</header>
 
-	<div id="engadir-fotos-input" class="zona-dragdrop hidden" data-url="<?php echo $this->App->url; ?>subir-fotos/<?php echo $galeria['nome']; ?>" data-maxfilesize="<?php echo intval(ini_get('upload_max_filesize')); ?>">
+	<div id="engadir-fotos-input" class="zona-dragdrop hidden" data-url="<?php echo $this->App->url; ?>subir-fotos/<?php echo $galeria['nome']; ?>" data-maxfilesize="<?php echo $max; ?>">
 		Arrastra aqui as fotos que queiras subir
-		<p>Solo imaxes (jpg). Máximo 100 megas e 100 arquivos ao mesmo tempo</p>
+		<p>Solo imaxes (jpg). Máximo 200 arquivos ao mesmo tempo e <?php echo $max; ?>Mb cada un.</p>
 
 		<progress max="100" class="hidden"></progress>
 
